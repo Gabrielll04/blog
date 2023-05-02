09906f17d05792e6c06c4eae00aa7786f9b71c74
@Gabrielll04 <gabrielggcarneiro04@gmail.com>
2023-05-02 14:48:31 -0300

    ~ Adding darkmode

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,63 @@
+import { useEffect, useState } from 'react'
 import { SiOpenaigym } from 'react-icons/si'
 import { TbBrandTypescript } from 'react-icons/tb'
 import { TbBrandNextjs } from 'react-icons/tb'
-import { TbMoonStars, TbMoonFilled } from 'react-icons/tb'
+import { TbMoonStars } from 'react-icons/tb' 
 
 import SideBar from './Components/SideBar'
+
 function App() {
+  const [darkMode, setDarkMode] = useState<boolean>(false)
+
+  function themeToggle(): void {
+    localStorage.setItem("dark-mode", "false")
+
+    if (!darkMode) {
+      localStorage.setItem("dark-mode", "true")
+    } else {
+      localStorage.setItem("dark-mode", "false")
+    }
+
+    let darkModeToggle: any = localStorage.getItem("dark-mode")
+    if (darkModeToggle != null) {
+      const parsedDarkModeToggle: boolean = JSON.parse(darkModeToggle)
+      setDarkMode(parsedDarkModeToggle)
+    }
+  }
+
+  // useEffect((): void => {
+  //   let darkModeToggle: any = localStorage.getItem("dark-mode")
+  //   if (darkModeToggle != null) {
+  //     const parsedDarkModeToggle: boolean = JSON.parse(darkModeToggle)
+  //     setDarkMode(parsedDarkModeToggle)
+  //   }
+  // })
+
   return (
-    <div className='flex flex-col w-screen min-h-screen max-h-none'>
+    <div className={`flex flex-col w-screen h-screen max-h-none transition ease-in-out delay-400 ${darkMode ? "dark" : ""}`}>
       <SideBar />
-      <div className='flex my-auto sm:flex-col justify-center items-center px-8 sm:px-0'>
+      <div className='flex my-10 sm:my-auto sm:flex-col justify-center items-center px-8 sm:px-0'>
         <div className='flex sm:flex-row flex-col items-center w-[66rem] sm:space-x-5'>
-          <div className='flex justify-center items-center sm:w-[33rem] sm:h-[33rem] hover:ring-zinc-300'><SiOpenaigym className='w-32 h-32 sm:w-96 sm:h-96 sm:mb-9 text-green-500' /></div>
+          <div className='flex justify-center items-center sm:w-[33rem] sm:h-[33rem] hover:ring-zinc-300'><SiOpenaigym className='w-52 h-52 sm:w-96 sm:h-96 sm:mb-9 text-green-500' /></div>
           <div className='flex flex-col mb-10 space-y-10'>
             <div className='flex flex-col sm:items-start items-center sm:space-y-3'>
               <span className='sm:font-bold sm:text-5xl invisible sm:visible'>gabriel.dev</span>
               <span className='text-xl text-center sm:text-start'>Using my passion to solve your problems</span>
             </div>
             <div className='flex flex-col h-52 space-y-3 items-center sm:items-start'>
-              <div className='land-button ring-green-200 bg-green-100 cursor-pointer'>
-                <TbBrandTypescript className='text-green-400 text-4xl' />
-                <span className=''>Blog</span>
+              <div className='land-button ring-green-200 dark:ring-green-900 bg-green-100 cursor-pointer dark:bg-green-400'>
+                <TbBrandTypescript className='text-green-700 text-4xl' />
+                <span className='font-semibold text-xl'>Blog</span>
               </div>
-              <div className='land-button ring-indigo-200 bg-indigo-100 cursor-pointer'>
-                <TbBrandNextjs className='text-indigo-400 text-4xl' />
-                <span className=''>Next.js</span>
+              <div className='land-button ring-indigo-200 dark:ring-indigo-900 bg-indigo-100 cursor-pointer dark:bg-indigo-400'>
+                <TbBrandNextjs className='text-indigo-700 text-4xl' />
+                <span className='font-semibold text-xl'>Next.js</span>
               </div>
             </div>
           </div>
         </div>
       </div>
-      <button className='self-end text-3xl mr-3 mb-3 sm:mr-10 sm:mb-7 rounded-full shadow-inner font-bold'><TbMoonStars /></button>
+      <button className='self-end text-3xl mr-3 mb-3 sm:mr-10 sm:mb-7 rounded-full shadow-inner' onClick={() => themeToggle()}>{darkMode }<TbMoonStars /></button>
     </div>
   );
 }
--- src/Components/SideBar.tsx
+++ src/Components/SideBar.tsx
@@ -4,7 +4,7 @@ import { Menu, Transition } from '@headlessui/react'
 function MyDropdown() {
   return (
     <Menu>
-      <Menu.Button><TbMenu2 className='flex text-2xl items-end'/></Menu.Button>
+      <Menu.Button><TbMenu2 className='flex text-3xl items-end'/></Menu.Button>
 
       {/* Use the `Transition` component. */}
       <Transition
@@ -15,7 +15,8 @@ function MyDropdown() {
         leaveFrom="transform scale-100 opacity-100"
         leaveTo="transform scale-95 opacity-0"
       >
-        <Menu.Items className="flex flex-col w-24 origin-right absolute right-0 bg-slate-200 px-2 rounded ring-1 ring-slate-300">
+        <Menu.Items className="flex flex-col w-32 origin-right absolute right-0 bg-zinc-100 ring-1 ring-zinc-200 p-1 rounded">
+          <Menu.Item><span className='dropItem'>item</span></Menu.Item>
           <Menu.Item><span className='dropItem'>item</span></Menu.Item>
           <Menu.Item><span className='dropItem'>item</span></Menu.Item>
         </Menu.Items>
@@ -26,8 +27,8 @@ function MyDropdown() {
 
 export default function SideBar() {
   return (
-    <div className='flex items-center justify-around w-screen h-12 sm:h-20 z-10 sm:text-xl sm:px-0 px-3 '>
-      <span className='text-lg font-semibold sm:font-bold sm:text-2xl'>gabriel.dev</span>
+    <div className='flex items-center justify-around w-full h-12 sm:h-20 z-10 sm:text-xl sm:px-0 px-3'>
+      <span className='text-lg font-bold sm:font-bold sm:text-3xl'>gabriel.dev</span>
       <div className='flex sm:space-x-20'>
         <button className='navButton'>items</button>
         <button className='navButton'>items</button>
@@ -35,7 +36,7 @@ export default function SideBar() {
       </div>
       <button><TbBrandGithub className='text-5xl sm:hover:scale-110 transition invisible sm:visible'/></button>
       {/* <button className='' ><TbMenu2 className='text-2xl'/></button> */}
-      {<div className='sm:hidden'>{MyDropdown()}</div>}
+      {<div className='sm:hidden'>{ MyDropdown() }</div>}
     </div>
   )
 } 
