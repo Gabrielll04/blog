e7ceeb75911a378cf9bce76a6df0cd9689ce9b95
Gabrielll04 <gabrielggcarneiro04@gmail.com>
2023-05-24 11:00:08 -0300

    Changing the ode formating

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
-import { useState } from "react";
-import {
-  SiOpenaigym,
-  SiLinkedin,
-  SiTwitter,
-  SiGithub,
-  SiInstagram,
-} from "react-icons/si";
-import {
-  TbAlignBoxLeftTop,
-  TbAddressBook,
-  TbMoonStars,
-  TbSun,
-} from "react-icons/tb";
+import { useState } from "react"
+import { SiOpenaigym } from "react-icons/si"
+import { TbAlignBoxLeftTop, TbAddressBook } from "react-icons/tb"
+import SocialButtons from "./Components/social-buttons"
+import ThemeToggle from "./lib/theme-toggle"
+import ThemeButton from "./Components/ui/theme-button"
+import getTheme from "./lib/get-theme"
+import SideBar from "./Components/side-bar"
 
 function App() {
   let darkModeToggle: any = localStorage.getItem("dark-mode"); //getting the localstorage data
@@ -38,25 +32,31 @@ function App() {
   }
 
   return (
-      <div className={`flex flex-col max-w-none max-h-none min-h-screen transition ease-in-out delay-400 ${darkMode ? "dark" : ""}`}>
-        <div className="flex sm:flex-col justify-center items-center px-8 sm:px-0 sm:my-auto ">
-          <div className="flex my-auto sm:flex-row flex-col items-center w-[66rem] sm:space-x-5">
-            <div className="flex justify-center items-center sm:w-[33rem] sm:h-[33rem] hover:ring-zinc-300">
-              <SiOpenaigym className="w-52 h-52 sm:w-96 sm:h-96 text-green-500" />
+    <main className={`flex flex-col max-w-none max-h-none min-h-screen transition ease-in-out delay-400 ${darkMode ? "dark" : ""}`}>
+      <SideBar/>
+      <div className="flex sm:flex-col justify-center items-center px-8 sm:px-0 sm:my-auto ">
+        <div className="flex my-auto sm:flex-row flex-col items-center w-[66rem] sm:space-x-5">
+          <div className="flex justify-center items-center sm:w-[33rem] sm:h-[33rem] hover:ring-zinc-300">
+            <SiOpenaigym className="w-52 h-52 sm:w-96 sm:h-96 text-green-500" />
+          </div>
+          <div className="flex flex-col space-y-10">
+            <div className="flex flex-col sm:items-start items-center sm:space-y-3">
+              <span className="sm:font-bold sm:text-5xl invisible sm:visible">
+                gabriel.dev
+              </span>
+              <span className="text-lg text-center sm:text-start w-96">
+                Welcome to my personal site! I'am currently studying Software
+                Engineering and i am learning about various
+                technologies such as Docker, TS, Next13 and NixOS. Additionally, i
+                have been exploring function programming language like Nix
+                and Elixir! Please fell free to visit my portfolio and blogs
+                using the links below.
+              </span>
             </div>
-            <div className="flex flex-col space-y-10">
-              <div className="flex flex-col sm:items-start items-center sm:space-y-3">
-                <span className="sm:font-bold sm:text-5xl invisible sm:visible">
-                  gabriel.dev
-                </span>
-                <span className="text-lg text-center sm:text-start w-96">
-                  Welcome to my personal site! I'am currently studying Software
-                  Engineering and i am learning about various
-                  technologies such as Docker, TS, Next13 and NixOS. Additionally, i
-                  have been exploring function programming language like Nix
-                  and Elixir! Please fell free to visit my portfolio and blogs
-                  using the links below.
-                </span>
+            <div className="flex flex-col h-52 items-center sm:items-start">
+              <div className="land-button ring-green-200 dark:ring-green-900 bg-green-100 cursor-pointer dark:bg-green-400 mb-3 animate-button">
+                <TbAddressBook className="text-green-700 text-4xl" />
+                <span className="font-semibold text-xl">Portfolio</span>
               </div>
               <div className="flex flex-col h-52 items-center sm:items-start">
                 <div className="land-button ring-green-200 dark:ring-green-900 bg-green-100 cursor-pointer dark:bg-green-400 mb-3 animate-button">
